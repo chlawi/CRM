d331ce41ad95a8f8f8f5f002942ffed8aec79c02
Chet Williams <Chet.Williams@l-tron.com>
2015-02-27 15:11:48 -0500

    added authorization to register

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ var jwtSecret = 'DotheRightThing';
 
 var moment = require('moment');
 
-
 var app = express();
 
 app.use(logger('dev'));
@@ -28,20 +27,34 @@ app.use((cors()));
 
 app.use('/', express.static(path.join(__dirname, "/../client")));
 
+app.use(expressJwt({
+  secret: 'DotheRightThing',
+  credentialsRequired: true,
+  getToken: function fromHeaderOrQuerystring (req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+      return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
+    }
+    return null;
+  }
+}).unless({path: ['/api/signIn']}));
+
+//app.use(expressJwt({secret: jwtSecret}).unless({path: ['/api/signIn','/api/register']}));
+
 app.use(function (req, res, next) {
-  console.log("IP Addresses: %s, Hostname: %s, path: %s, base: %s", req.ip, req.hostname, req.path, req.baseUrl);
+  console.log("IP Addresses: %s, Hostname: %s, path: %s, base: %s", req.ip, req.hostname, req.path, req.baseUrl, req.user);
   return next()
 });
 
-app.use(expressJwt({secret: jwtSecret}).unless({path: ['/api/signIn']}));
 
 var db = mongoskin.db("mongodb://localhost:27017/l-tronInside", {safe: true});
 
 app.post('/api/signIn', function (req, res, next) {
   try {
-
-    if (!( req.body.email && req.body.password))  {
-      res.send(401, 'Invalid user name or password');
+    console.log('request was here!', req.body);
+    if (!( req.body.email && req.body.password)) {
+      res.status(401).send('User name and password required');
     }
     //else{
     //  res.status(200).end();
@@ -55,7 +68,7 @@ app.post('/api/signIn', function (req, res, next) {
         throw(err);
       }
       else if (result === null) {
-        res.status(401).end()
+        res.status(401).end('Invalid user name or password')
       }
       else {
         var user = result;
@@ -65,12 +78,14 @@ app.post('/api/signIn', function (req, res, next) {
         if (user.password === hashedPassword) {
           var expiration = moment().add(7, 'days').valueOf();
           var token = jwt.sign(user.email, jwtSecret, expiration);
-
           var profile = {
             firstName: user.firstName,
             role: user.role,
             alias: user.alias
           };
+
+          var decoded = jwt.decode(token);
+          console.log(decoded);
 
           res.send({
             token: token,
@@ -78,7 +93,7 @@ app.post('/api/signIn', function (req, res, next) {
           });
         }
         else {
-          res.status(401).end()
+          res.status(401).end('Invalid user name or password');
         }
       }
     });
@@ -88,6 +103,49 @@ app.post('/api/signIn', function (req, res, next) {
   }
 });
 
+app.post('/api/register', function (req, res, next) {
+console.log("in api/register", req.user)
+  db.collection('users').findOne({"email":req.user},{},function (err, result) {
+    if (err) {
+      console.log('and error occurred');
+      return next(err);
+    }
+
+    console.log(' no error occurred');
+    //console.log('result: ', result);
+    if (!(result && result.role === 'administrator')) {
+      res.status(403).send('user not authorized');
+    }
+    else{
+      console.log(' user authorized');
+      console.log(' pass db.collections');
+      var salt = crypto.randomBytes(128).toString('base64');
+      var hmac = crypto.createHmac('sha1', salt);
+
+      var hashed_pwd = hmac.update(req.body.password).digest('hex');
+
+      db.collection('users').insert({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        role: req.body.role,
+        alias: req.body.alias,
+        salt: salt,
+        password: hashed_pwd
+      }, function (e, results) {
+        if (e) {
+          console.log(e);
+          return next(e)
+        }
+        else {
+          console.log(results);
+          res.status(200).json(results)
+        }
+      })
+    }
+  });
+});
+
 app.param('collectionName', function (req, res, next, collectionName) {
   req.collection = db.collection(collectionName);
   return next()
@@ -95,7 +153,7 @@ app.param('collectionName', function (req, res, next, collectionName) {
 
 app.get('/api/:collectionName', function (req, res, next) {
   req.collection.find({}, {limit: 10, sort: [['_id', -1]]}).toArray(function (e, results) {
-    if (e) return next(e)
+    if (e) return next(e);
     res.send(results)
   })
 });
@@ -114,14 +172,14 @@ app.post('/api/:collectionName/signIn', function (req, res, next) {
       res.send(err)
     }
     else {
-      var user = {}
+      var user = {};
 
-      user.password = result.password
-      user.salt = result.salt
+      user.password = result.password;
+      user.salt = result.salt;
 
-      var hmac = crypto.createHmac('sha1', user.salt)
+      var hmac = crypto.createHmac('sha1', user.salt);
 
-      var hashed_pwd = hmac.update(req.body.password).digest('hex')
+      var hashed_pwd = hmac.update(req.body.password).digest('hex');
 
       if (user.password === hashed_pwd) {
         res.status(200).json(user)
@@ -131,35 +189,13 @@ app.post('/api/:collectionName/signIn', function (req, res, next) {
       }
     }
   })
-})
+});
 
-
-app.post('/api/:collectionName/register', function (req, res, next) {
-
-  var salt = crypto.randomBytes(128).toString('base64')
-  var hmac = crypto.createHmac('sha1', salt)
-
-  var hashed_pwd = hmac.update(req.body.password).digest('hex')
-
-  req.collection.insert({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    role: req.body.role,
-    alias: req.body.alias,
-    salt: salt,
-    password: hashed_pwd
-  }, function (e, results) {
-    if (e) {
-      console.log(e)
-      return next(e)
-    }
-    else {
-      console.log(results)
-      res.status(200).json(results)
-    }
-  })
-})
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.send(401, 'invalid token...');
+  }
+});
 
 app.use(function (err, req, res, next) {
   res.status(err.status || 500).json(err);
